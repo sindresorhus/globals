4a22639b52c9889dfaba1412ef3271de39d610a3
Sindre Sorhus <sindresorhus@gmail.com>
2017-12-09 13:50:27 +0100

    Enforce correct alphabetical order

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 import test from 'ava';
 import m from '.';
 
-test(t => {
+test('main', t => {
 	t.is(typeof m, 'object');
 	t.true(Object.keys(m).length > 10 && Object.keys(m).length < 1000);
+});
+
+test('ensure alphabetical order', t => {
+	for (const env of Object.keys(m)) {
+		const keys = Object.keys(m[env]);
+		t.deepEqual(keys.slice(), keys.sort((a, b) => a.localeCompare(b)), `The \`${env}\` keys don't have the correct alphabetical order`);
+	}
 });
