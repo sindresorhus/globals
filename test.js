90e5b21d9d7b60c4ea82bb5856d22b77c31d2192
Jan Krems <jankrems@google.com>
2020-02-07 11:39:32 -0800

    Add true globals for node

--- test.js
+++ test.js
@@ -12,3 +12,25 @@ test('ensure alphabetical order', t => {
 		t.deepEqual(keys.slice(), keys.sort((a, b) => a.localeCompare(b)), `The \`${env}\` keys don't have the correct alphabetical order`);
 	}
 });
+
+test('node is nodeBuiltin with commonjs arguments', t => {
+	// M.commonjs has global which isn't a commonjs argument and doesn't include
+	// __filename and __dirname which are.
+	const commonjsArgs = {
+		__dirname: false,
+		__filename: false,
+		exports: true,
+		module: false,
+		require: false
+	};
+
+	t.deepEqual({...m.nodeBuiltin, ...commonjsArgs}, m.node);
+
+	// Ensure that there's no overlap between true globals and the CommonJS arguments above.
+	for (const builtin of Object.keys(m.nodeBuiltin)) {
+		t.true(
+			commonjsArgs[builtin] === undefined,
+			`The builtin ${builtin} is not a CommonJS argument`
+		);
+	}
+});
