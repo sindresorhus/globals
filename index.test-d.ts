69f911dbc34d1b45fe4e9bc6619846a3c52b6173
Pelle Wessman <pelle@kodfabrik.se>
2024-02-09 12:19:23 +0100

    `xo --fix`

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,6 @@
 import {expectType, expectError} from 'tsd';
 import {ReadonlyDeep} from 'type-fest';
-import globals from './index.js';
+import globals from '.';
 
 expectType<ReadonlyDeep<Record<string, Record<string, boolean>>>>(globals);
 expectType<boolean>(globals.builtin.Array);
