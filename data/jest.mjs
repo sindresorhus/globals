334d475b3c86ae5f495467bc94d4f98cd9d8e183
fisker <lionkay@gmail.com>
2024-02-19 22:41:28 +0800

    Build data file

---
+++ data/jest.mjs
@@ -0,0 +1,18 @@
+export default {
+	"afterAll": false,
+	"afterEach": false,
+	"beforeAll": false,
+	"beforeEach": false,
+	"describe": false,
+	"expect": false,
+	"fdescribe": false,
+	"fit": false,
+	"it": false,
+	"jest": false,
+	"pit": false,
+	"require": false,
+	"test": false,
+	"xdescribe": false,
+	"xit": false,
+	"xtest": false
+}
---
+++ scripts/generate-data.mjs
@@ -0,0 +1,71 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+import {readGlobals} from './utilities.mjs';
+
+const DATA_FILE = new URL('../globals.json', import.meta.url);
+
+// List this to make sure we won't break `globals.json`
+// use `fs.readdir` later
+const environments = [
+	'builtin',
+	'es5',
+	'es2015',
+	'es2017',
+	'es2020',
+	'es2021',
+	'browser',
+	'worker',
+	'node',
+	'nodeBuiltin',
+	'commonjs',
+	'amd',
+	'mocha',
+	'jasmine',
+	'jest',
+	'qunit',
+	'phantomjs',
+	'couch',
+	'rhino',
+	'nashorn',
+	'wsh',
+	'jquery',
+	'yui',
+	'shelljs',
+	'prototypejs',
+	'meteor',
+	'mongo',
+	'applescript',
+	'serviceworker',
+	'atomtest',
+	'embertest',
+	'protractor',
+	'shared-node-browser',
+	'webextensions',
+	'greasemonkey',
+	'devtools',
+];
+
+async function getEnvironmentData(filename) {
+
+	const data = Object.fromEntries(
+		globals.map(
+			property => typeof property === 'string' ? [property, false] : [property.name, property.writable],
+		),
+	);
+
+	return {
+		environmentName,
+		globals: data,
+	};
+}
+
+async function getData() {
+	const data = await Promise.all(
+		environments.map(async environment => ({environment, globals: readGlobals(environment)}))
+	);
+
+	return Object.fromEntries(data.map(({environment, globals}) => [environment, globals]));
+}
+
+const data = await getData();
+await fs.writeFile(DATA_FILE, JSON.stringify(data, undefined, '\t') + '\n');
