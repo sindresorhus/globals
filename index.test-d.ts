4ff98bc5d71137db3a7844fe5761051cb08c8dba
fisker Cheung <lionkay@gmail.com>
2024-02-19 19:05:17 +0800

    Update dependencies (#212)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,6 @@
 import {expectType, expectError, expectAssignable} from 'tsd';
-import {ReadonlyDeep} from 'type-fest';
-import globals from '.';
+import {type ReadonlyDeep} from 'type-fest';
+import globals from './index.js';
 
 expectAssignable<ReadonlyDeep<Record<string, Record<string, boolean>>>>(globals);
 expectType<false>(globals.builtin.Array);
