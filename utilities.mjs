d86d13dcad8f3a6506f9d325e2c0f89af5da6dc1
fisker <lionkay@gmail.com>
2024-02-20 17:01:27 +0800

    Style

--- utilities.mjs
+++ utilities.mjs
@@ -33,5 +33,8 @@ function getIntersectionGlobals(globalsA, globalsB) {
 }
 
 export {
-	unique, sortObject, mergeGlobals, getIntersectionGlobals,
+	unique,
+	sortObject,
+	mergeGlobals,
+	getIntersectionGlobals,
 };
