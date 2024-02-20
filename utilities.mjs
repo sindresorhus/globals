54a4a2c616e087901d081fdd732b687da6800a8f
fisker <lionkay@gmail.com>
2024-02-20 16:36:39 +0800

    Generate `es2015` from `es5`

---
+++ utilities.mjs
@@ -0,0 +1,22 @@
+const sortObject = object =>
+	Object.fromEntries(
+		Object.entries(object).sort(([propertyA], [propertyB]) =>
+			propertyA.localeCompare(propertyB),
+		),
+	);
+
+function mergeGlobals(globalsA, globalsB) {
+	const existsInA = Object.keys(globalsB).filter(name => Object.hasOwn(globalsA, name));
+	if (existsInA.length > 0) {
+		throw new Error(`Already exits:\n${existsInA.map(name => ` - ${name}`).join('\n')}`)
+	}
+
+	const existsInB = Object.keys(globalsA).filter(name => Object.hasOwn(globalsB, name));
+	if (existsInB.length > 0) {
+		throw new Error(`Already exits:\n${existsInB.map(name => ` - ${name}`).join('\n')}`)
+	}
+
+	return sortObject({...globalsA, ...globalsB});
+}
+
+export {mergeGlobals};
