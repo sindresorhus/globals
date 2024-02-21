7ad7603798c0dc10183eed05e8c12d98659d4c83
fisker Cheung <lionkay@gmail.com>
2024-02-21 14:12:30 +0800

    Update scripts/get-jest-globals.mjs
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- scripts/get-jest-globals.mjs
+++ scripts/get-jest-globals.mjs
@@ -1,7 +1,7 @@
 import module from 'node:module';
 
 // https://github.com/jest-community/eslint-plugin-jest/blob/main/src/globals.json
-export default function getShelljsGlobals() {
+export default function getJestGlobals() {
 	const require = module.createRequire(import.meta.url);
 	const globals = require('eslint-plugin-jest/lib/globals.json');
 	return globals;
