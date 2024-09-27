1f47618596e44ccdbc6d813af9e9bfcdbcdcaeab
fisker <lionkay@gmail.com>
2024-09-28 00:13:51 +0800

    Linting

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -4,16 +4,12 @@
 module.exports = {
 	skipDownload: true,
 	chrome: {
-		// Bug? `latest` version can't be launched
-		// version: 'latest',
 		skipDownload: false,
 	},
 	'chrome-headless-shell': {
-		// version: 'latest',
 		skipDownload: false,
 	},
 	firefox: {
-		// version: 'latest',
 		skipDownload: false,
 	},
 };
