742789c92a4b1d3d782af9b3b9f083574b40bb7e
fisker <lionkay@gmail.com>
2026-06-22 21:21:09 +0800

    Workaround

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -7,7 +7,7 @@ const IS_CI = Boolean(process.env.CI);
 
 // Will download when execute
 module.exports = {
-	// skipDownload: true,
+	skipDownload: !IS_CI,
 	cacheDirectory: IS_CI ? path.join(__dirname, '.cache/puppeteer/') : undefined,
 	chrome: {
 		version: 'latest',
