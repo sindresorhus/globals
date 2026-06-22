1fed93ad523417ea1a6939c18a20d313f9446a4f
fisker <lionkay@gmail.com>
2026-06-22 20:15:45 +0800

    attempt

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -8,5 +8,5 @@ const IS_CI = Boolean(process.env.CI);
 // Will download when execute
 module.exports = {
 	skipDownload: true,
-	cacheDirectory: IS_CI ? path.join(__dirname, '.cache/puppeteer/') : undefined,
+	cacheDirectory: IS_CI ? path.join(__dirname, '.cache/puppeteer') : undefined,
 };
