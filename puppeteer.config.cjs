99e2a5724fa95da82568730d599e5bf0bb90c3b9
fisker <lionkay@gmail.com>
2026-06-22 20:36:48 +0800

    Install with command

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -7,8 +7,6 @@ const IS_CI = Boolean(process.env.CI);
 
 // Will download when execute
 module.exports = {
-	// Workaround for https://github.com/puppeteer/puppeteer/pull/15130
-	// Use `skipDownload: true`
-	skipDownload: !IS_CI,
+	skipDownload: true,
 	cacheDirectory: IS_CI ? path.join(__dirname, '.cache/puppeteer/') : undefined,
 };
